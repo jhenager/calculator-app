962b516600ca995ac869701ab276013c04e5464a
James Henager <jameschenager@gmail.com>
2020-06-01 11:56:33 -0700

    initial commit
    Co-authored-by: Mook Dang <mookdang24@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const add = function(number1, number2) {
+  return number1 + number2;
+  };
+  
+  const number1 = parseFloat(prompt("Enter a number:"));
+  const number2 = parseFloat(prompt("Enter another number:"));
+  
+  alert(add(number1, number2));
+  

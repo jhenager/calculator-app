7a13ad0108dc55c22bcde26d32d91892bb332dd8
James Henager <jameschenager@gmail.com>
2020-06-03 14:56:51 -0700

    add output element to display results in ui
    Co-authored-by: Jeff Kim <kim2jy@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
+// This is business logic
+const add = function(number1, number2) {
+  return number1 + number2;
+};
+
+const subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
 const multiply = function(number1, number2) {
   return number1 * number2;
-  };
-  
-  const number1 = parseFloat(prompt("Enter a number:"));
-  const number2 = parseFloat(prompt("Enter another number:"));
-  const result = multiply(number1, number2);
-  alert(result);
-  
+};
+
+const divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// This is user interface logic:
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+    
+  });
+});
